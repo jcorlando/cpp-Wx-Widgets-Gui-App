c657d0984348840e7f6c86919d9b5b4d5beb6077
Juan Orlando <jcorlandomc@yahoo.com>
2020-06-08 16:21:56 -0700

    added window app compilation

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-g++ -std=c++17 -Wall -Wextra -pedantic -o main.exe MyFrame.cpp MyApp.cpp -I/c/wxWidgets-3.1.3/lib/gcc_dll/mswu -I/c/wxWidgets-3.1.3/include -I/c/wxWidgets-3.1.3/include/msvc -L/c/wxWidgets-3.1.3/lib/gcc_dll -lwxbase31u -lwxmsw31u_core
+g++ -std=c++17 -Wall -Wextra -pedantic -mwindows -o main.exe MyFrame.cpp MyApp.cpp -I/c/wxWidgets-3.1.3/lib/gcc_dll/mswu -I/c/wxWidgets-3.1.3/include -I/c/wxWidgets-3.1.3/include/msvc -L/c/wxWidgets-3.1.3/lib/gcc_dll -lwxbase31u -lwxmsw31u_core
 
 
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 main.exe: MyApp.cpp MyFrame.cpp
 	./build.sh
 clean:
-	rm main.exe && touch cApp.cpp cMain.cpp
+	rm main.exe && touch MyApp.cpp MyFrame.cpp
